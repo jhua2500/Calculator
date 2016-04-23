a153d3f4e5e23e398c84ef1c78c4c77cdbebd580
Joey Hua <theonlyjoeyhua@gmail.com>
2016-04-22 19:52:23 -0700

    Update Axes.java
    Revamped axes drawing method to try to fix errors in precision, uses recursion instead.
    Did not work, the graphed equations are still not completely centered with the axes.

--- Axes.java
+++ Axes.java
@@ -1,29 +1,128 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
 
 public class Axes extends JPanel {
+   ArrayList<Double> xVals = new ArrayList<Double>(), yVals = new ArrayList<Double>(), 
+		   			  list = new ArrayList<Double>();
+   int pass = 1;
       
    public Axes()
-   {
-	setOpaque( false );
+   {	   
+	   setOpaque( false );
    }
-
+   
+   public double getxScale() { return (double)getWidth()/32; }
+   public double getyScale() { return (double)getHeight()/32; }
+   
+   public ArrayList<Double> lines(double x)
+   {
+	   double w = 0;
+	   
+	   if (pass == 5) {
+		   w = x;
+		   x /= 2;
+		   pass = 1;
+		   for (int j = 0; j < 17; j++)
+			   list.add(x+w*j);
+	   }
+	   else if (pass == 1) {
+		   w = x;
+		   x /= 2;
+		   pass++;
+		   list.add(x);
+		   return lines(x);
+	   }
+	   else if (pass == 2) {
+		   w = x;
+		   x /= 2;
+		   list.add(x); list.add(x+w);
+		   pass++;
+		   return lines(x);
+	   }
+	   else if (pass == 3) {
+		   w = x;
+		   x /= 2;
+		   list.add(x); list.add(x+w); list.add(x+2*w); list.add(x+3*w);
+		   pass++;
+		   return lines(x);
+	   }
+	   else if (pass == 4) {
+		   w = x;
+		   x /= 2;
+		   for (int j = 0; j < 8; j++) 
+			   list.add(x+j*w);
+		   pass++;
+		   return lines(x);
+	   }
+	   else if (pass == 4) {
+		   w = x;
+		   x /= 2;
+		   for (int j = 0; j < 8; j++) 
+			   list.add(x+j*w);
+		   pass++;
+		   return lines(x);
+	   }
+	   pass = 1;
+	   return list; 
+   }
+   
    public void paintComponent( Graphics g )
    {
-	super.paintComponent( g );
-	g.drawLine( getWidth() / 2, 0,  getWidth() / 2, getHeight() );
-	g.drawLine( 0, getHeight() / 2, getWidth(), getHeight() / 2 );
+	   double x=0, y=0;
+	   super.paintComponent(g);
+	   Graphics2D g2 = (Graphics2D) g;
 
-	for ( int i = 1; i <= 100; i++ ) 
-	   g.drawString( "|", ( getWidth() / 2 ) + ( 20 * i ), ( getHeight() / 2 ) + 5 );
-	
-	for ( int j = 1; j <= 100; j++ ) 
-	   g.drawString( "--", ( getWidth() / 2 ) - 3, ( getHeight() / 2 ) - ( 20 * j ) );
+	   xVals.clear();
+	   xVals = lines((double)getWidth());
+	   for (int j = 0; j < xVals.size(); j++) {
+		   if (xVals.get(j) == (double)getWidth()/2) {
+			   g2.setColor(Color.black);
+			   g2.draw(new Line2D.Double(xVals.get(j), 0, xVals.get(j), getHeight()));
+			   x = xVals.get(j);
+		   }
+		   else {
+			   g2.setColor(Color.lightGray);
+			   g2.draw(new Line2D.Double(xVals.get(j), 0, xVals.get(j), getHeight()));
+		   }
+	   }
+	   
+	   yVals.clear();
+	   yVals = lines((double)getHeight());
+	   for (int h = 0; h < yVals.size(); h++) {
+		   if (yVals.get(h) == (double)getHeight()/2) {
+			   g2.setColor(Color.black);
+			   g2.draw(new Line2D.Double(0, yVals.get(h), getWidth(), yVals.get(h)));
+			   y = yVals.get(h);
+		   }
+		   else {
+			   g2.setColor(Color.lightGray);
+			   g2.draw(new Line2D.Double(0, yVals.get(h), getWidth(), yVals.get(h)));
+		   }
+       }
+	   System.out.println("Axes:");
+	   System.out.println(getxScale());
+	   System.out.println(getyScale());
+	   System.out.println("x:"+x);
+	   System.out.println("y:"+y+"\n");
+	   
+	   //PREVIOUS METHOD FOR DRAWING AXES:
+	   /*double xScale = getWidth()/32, yScale = getHeight()/32;	
+	   super.paintComponent(g);
+	   Graphics2D g2 = ( Graphics2D ) g;
 
-	for ( int k = 1; k <= 100; k++ )
-	   g.drawString( "|", ( getWidth() / 2 ) - ( 20 * k ), ( getHeight() / 2 ) + 5 );
-
-	for ( int m = 1; m <= 100; m++ )
-	   g.drawString( "--", ( getWidth() / 2 ) - 3, ( getHeight() / 2 ) + ( 20 * m ) ); 
-   }
+	   g2.setColor(Color.lightGray);
+	   for (double x = 0; x < getWidth(); x+=xScale) 
+			   g2.draw(new Line2D.Double(x, 0, x, getHeight()));
+	   
+	   for (double y = 0; y < getHeight(); y+=yScale) 			   
+			   g2.draw(new Line2D.Double(0, y, getWidth(), y));
+			   
+	   g2.setColor(Color.black);
+	   g2.draw(new Line2D.Double(getWidth()/2, 0, getWidth()/2, getHeight()));
+	   g2.draw(new Line2D.Double(0, getHeight()/2, getWidth(), getHeight()/2));
+	   //g2.draw(new Line2D.Double(xScale*25, 0, xScale*25, getHeight()));
+	   //g2.draw(new Line2D.Double(0, yScale*25, getWidth(), yScale*25));*/
+   }	   
 } 
