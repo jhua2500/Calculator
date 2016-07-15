4a85db9a7a5ea540353099cdb68f4a4e11c3e9e0
Joey Hua <theonlyjoeyhua@gmail.com>
2016-07-14 20:02:08 -0700

    Update ModForExponents.java

--- ModForExponents.java
+++ ModForExponents.java
@@ -1,87 +1,20 @@
+import java.util.ArrayList;
+
 public class ModForExponents {
-	String[] newNumArray, newOpArray;
-	int loop = 1;
+	int i = -1;
 		
-	public void modify(String[] numArray, String[] opArray)
+	public void modify(ArrayList<String> nList, ArrayList<String> oList)
 	{
-		String operator = "";
 		double x = 0, y = 0;
-		for(int i = 0; i <= opArray.length; i++) {
-			if(i == opArray.length) {
-				if(loop == 1) {
-					loop++;
-					modify(numArray, opArray);
-				}
-				else {
-					newNumArray = numArray;
-					newOpArray = opArray;
-					return;
-				}
-			}
-			else {
-				operator = opArray[i];
-				try {
-			    	x = Double.parseDouble(numArray[i]);
-			    }
-			    catch(NumberFormatException e) {
-			    	if(loop == 1) {
-						loop++;
-						modify(numArray, opArray);
-					}
-					else {
-						newNumArray = numArray;
-						newOpArray = opArray;
-						return;
-					}
-			    }			 
-			    try {
-			    	y = Double.parseDouble(numArray[i+1]);
-			    }
-			    catch(NumberFormatException e) {
-			    	if(loop == 1) {
-						loop++;
-						modify(numArray, opArray);
-					}
-					else {
-						newNumArray = numArray;
-						newOpArray = opArray;
-						return;
-					}
-			    }
-			}
-			switch(operator) {
-				case "^": 
-					numArray[i] = Double.toString(Math.pow(x,y));
-					for (int p = i; p < opArray.length; p++) {
-						if(p == opArray.length - 1) {
-							opArray[p] = "n";
-							break;
-						}
-						else if(p == opArray.length - 2) {
-							opArray[p] = opArray[p + 1];
-							opArray[p + 1] = "n";
-							break;
-						}
-					    else {
-						    opArray[p] = opArray[p + 1];
-						    opArray[p + 1] = opArray[p + 2];
-					    }
-					}
-					for (int a = i; a < numArray.length - 1; a++) {
-						if(a == numArray.length - 2) {
-							numArray[a + 1] = "n";
-							break;
-						}
-						else {
-							numArray[a + 1] = numArray[a + 2]; 
-					    }
-				    }
-				    break;
-				case "n": 
-					break;
-				default: 
-					break;
-			}
+		
+		while (oList.contains("^")) {
+			i = oList.indexOf("^");		
+			x = Double.parseDouble(nList.get(i));
+			y = Double.parseDouble(nList.get(i+1));							
+			nList.set(i, Double.toString(Math.pow(x, y)));
+			nList.remove(i+1);
+			oList.remove(i);
 		}
+		return;
 	}
 }
